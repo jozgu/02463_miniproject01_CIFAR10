c27d859ccfc3b389da620adfa27840ef8fc03edb
Lachlan Houston <LachlanHouston111@gmail.com>
2023-03-02 09:50:55 +0100

    plotspush

--- Resultater framedium/framedium 2.py
+++ Resultater framedium/framedium 2.py
@@ -1,32 +1,14 @@
+#%%
 # Install GPy, GPyOpt
 
 import tensorflow as tf
 import tensorflow.keras as K
 import numpy as np
 import matplotlib.pyplot as plt
-#%matplotlib inline
 import GPy
-import GPyOpt
 from GPyOpt.methods import BayesianOptimization
 
-# print(tf.__version__)
-# #2.3.0
-# print(K.__version__)
-# #2.4.0
-# print(np.__version__)
-# #1.18.5
-# print(matplotlib.__version__)
-# #3.2.2
-# print(GPy.__version__)
-# #1.9.8
-# print(GPyOpt.__version__)
-# #1.2.1
 def preprocess_data(X, Y):
-    """
-    function that pre-processes the CIFAR10 dataset as per
-    densenet model requirements for input images
-    labels are one-hot encoded
-    """
     X = K.applications.densenet.preprocess_input(X)
     Y = K.utils.to_categorical(Y)
     return X, Y
@@ -34,7 +16,7 @@ def preprocess_data(X, Y):
 # load the Cifar10 dataset, 50,000 training images and 10,000 test images (here used as validation data)
 (x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
 
-x_train, y_train, x_test, y_test = x_train[:100], y_train[:100], x_test[:20], y_test[:20]
+x_train, y_train, x_test, y_test = x_train[:1000], y_train[:1000], x_test[:200], y_test[:200]
 
 x_train, y_train = preprocess_data(x_train, y_train)
 x_test, y_test = preprocess_data(x_test, y_test)
@@ -67,7 +49,7 @@ val_generator = val_datagen.flow(x_test,
                                  shuffle=False)
 features_valid = base_model.predict(val_generator)
 
-def build_model(units=256, learning_rate=1e-4, l2=1e-3, activation=1, rate=0.7):
+def build_model(units, learning_rate, l2=1e-3, activation=1, rate=0.8):
   """function that builds a model for the head classifier"""
   # weights are initialized as per the he et al. method
   initializer = K.initializers.he_normal()
@@ -133,13 +115,10 @@ def evaluate_model(model):
   return evaluation
 
 # define the kernel for the Bayesian surrogate model using the Matern kernel
-kernel = GPy.kern.src.stationary.Matern52(input_dim=1, variance=1.0, lengthscale=1.0)
+kernel = GPy.kern.src.stationary.Matern52(input_dim=50, variance=3.0, lengthscale=2.0)
 # hyperparameter bounds
-bounds = [{'name': 'units', 'type': 'discrete', 'domain': (64, 128, 256, 512)},
-          {'name': 'learning_rate', 'type': 'discrete', 'domain': (1e-3, 1e-4, 1e-5, 1e-6)},
-          #{'name': 'l2', 'type': 'discrete', 'domain': (1e-1, 1e-2, 1e-3)},
-          #{'name': 'activation', 'type': 'discrete', 'domain': (1, 2, 3)}, #################################
-          #{'name': 'rate', 'type': 'discrete', 'domain': (0.3, 0.5, 0.7)}
+bounds = [{'name': 'units', 'type': 'discrete', 'domain': (8, 16, 32, 64, 128, 256, 512, 1024, 2048)},
+          {'name': 'learning_rate', 'type': 'discrete', 'domain': ((0.5)*1e-3, 1e-3, (1.5)*1e-3, (2)*1e-3, (2.5)*1e-3, (3)*1e-3, (3.5)*1e-3, (4)*1e-3, (4.5)*1e-3, (5)*1e-3, (5.5)*1e-3, (6)*1e-3, (6.5)*1e-3)},
           ]
 # Note: 'activation' domain parameters (1, 2, 3) correspond to strings ('relu', 'elu', 'tanh'); dictionary defined in build_model()
 # objective function for the model optimization:
@@ -156,9 +135,6 @@ def f(x):
   model, lr_reduce, early_stop, checkpoint = build_model(
                                         units=int(x[:,0]),
                                         learning_rate=float(x[:,1]),
-                                        # l2=float(x[:,2]),     ############################################
-                                        # activation=int(x[:,3]),
-                                        # rate=float(x[:,4])
                                         )
   history = fit_model(model, lr_reduce, early_stop, checkpoint)
   evaluation = evaluate_model(model)
@@ -166,7 +142,6 @@ def f(x):
   print("LOSS:\t{0} \t ACCURACY:\t{1}".format(evaluation[0],
   evaluation[1]))
   print(evaluation)
-  print()
   # compare previous and current validation accuracies
   if not previous_best_model:
     K.models.save_model(model, 'cifar10_best.h5', overwrite=False,
@@ -174,38 +149,14 @@ def f(x):
   if previous_best_model and evaluation[1] > previous_evaluation[1]:
     K.models.save_model(model, 'cifar10_best.h5', overwrite=True,
     include_optimizer=True)
-  # Get the dictionary containing each metric and the loss for each epoch
-  # history_dict = history.history
-  # print(history_dict)
-  def plot_history(history):
-    """function that plots the model loss and accuracy"""
-    plt.figure(1, figsize = (15,8))
-    plt.subplot(221)
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'valid'])
-    plt.subplot(222)
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'valid'])
-    # plt.show()
-  # plot the model accuracy and loss results
-  plot_history(history)
-  # delete the instantiated models from memory and clear the session
   del model
   del previous_best_model
   K.backend.clear_session()
   return evaluation[1]
 # Initializing X and Y, and adding noise (if need be)
-# X_init = np.array([[int(16)]])
-# Y_init = f(X_init)
-# noise = 0.2
+#X_init = np.array([[int(16)]])
+#Y_init = f(X_init)
+noise = 0.2
 opt = BayesianOptimization(f=f,
                                  domain=bounds,
                                  model_type='GP',
@@ -235,19 +186,6 @@ opt.save_report('bayes_opt.txt')
 
 # print optimized model
 activation_dict = {1: 'relu', 2: 'elu', 3: 'tanh'}
-print("""
-Optimized Parameters:
-\t{0}:\t{1}
-\t{2}:\t{3}
-\t{4}:\t{5}
-\t{6}:\t{7}
-\t{8}:\t{9}
-""".format(bounds[0]["name"], opt.x_opt[0],
-           bounds[1]["name"], opt.x_opt[1],
-          #  bounds[2]["name"], optimizer.x_opt[2],                  #######################################
-          #  bounds[3]["name"], activation_dict[optimizer.x_opt[3]],
-          #  bounds[4]["name"], optimizer.x_opt[4]
-          ))
 print("optimized accuracy: {0}".format(abs(opt.fx_opt)))
 
 
@@ -270,62 +208,7 @@ for line in lines:
 # %% FRA EX 4 efter "opt ="
 opt.acquisition.exploration_weight=0.5
 
-opt.run_optimization(max_iter = 15)
-
-x_best = opt.X[np.argmin(opt.Y)]
-print("The best parameters obtained: n_estimators=" + str(x_best[0]) + ", max_depth=" + str(x_best[1]) + ", max_features=" + str(
-    x_best[2])  + ", criterion=" + str(
-    x_best[3]))
-
-
-
-
-## comparison between random search and bayesian optimization
-## we can plot the maximum oob per iteration of the sequence
-
-# collect the maximum each iteration of BO, note that it is also provided by GPOpt in Y_Best
-y_bo = np.maximum.accumulate(-opt.Y).ravel()
-# define iteration number
-xs = np.arange(1,21,1)
-
-plt.plot(xs, max_oob_per_iteration, 'o-', color = 'red', label='Random Search')
-plt.plot(xs, y_bo, 'o-', color = 'cyan', label='Bayesian Optimization')
-plt.legend()
-plt.xlabel('Iterations')
-plt.ylabel('Out of bag error')
-plt.title('Comparison between Random Search and Bayesian Optimization')
-plt.show()
-
-
-
-
-#We start by taking a look a the model subclass of our Bayesian optimization object
-print(opt.model.input_dim)
-#so there is 6 dimensions, that is 1 for each of n_estimators and depth which are discrete variables.
-# the remaining 4 is because one-out-of-K encoding is using for the categorical variables max_features and criterion
-#we can also look at kernel parameters
-print(opt.model.get_model_parameters_names())
-#and get the current fitted values
-print(opt.model.get_model_parameters())
-#To get a plot of the acquisition function we use the function opt.acquisition.acquisition_function
-#first we define a sensible grid for the first to parameters 
-#indexing='ij' ensures that x/y axes are not flipped (which is default):
-#we also add two extra axes for the categorical varibles and here fix these to 0 ('log2' and 'gini')
-#note that the acqusition function can actually take any value not only integers as it lives in the GP space (here 0.5 intervals)
-#and it is quite fast to evaluate - here in 40000 points
-n_feat = np.arange(1,101,0.5)
-max_d = np.arange(10,110,0.5)
-pgrid = np.array(np.meshgrid(n_feat, max_d,[1],[0],[1],[0],indexing='ij'))
-print(pgrid.reshape(6,-1).T.shape)
-#we then unfold the 4D array and simply pass it to the acqusition function
-acq_img = opt.acquisition.acquisition_function(pgrid.reshape(6,-1).T)
-#it is typical to scale this between 0 and 1:
-acq_img = (-acq_img - np.min(-acq_img))/(np.max(-acq_img - np.min(-acq_img)))
-#then fold it back into an image and plot
-acq_img = acq_img.reshape(pgrid[0].shape[:2])
-plt.figure()
-plt.imshow(acq_img.T, origin='lower',extent=[n_feat[0],n_feat[-1],max_d[0],max_d[-1]])
-plt.colorbar()
-plt.xlabel('n_features')
-plt.ylabel('max_depth')
-plt.title('Acquisition function');
+#%%
+opt.run_optimization(max_iter = 25, verbosity = False)
+opt.plot_acquisition()
+opt.plot_convergence()
